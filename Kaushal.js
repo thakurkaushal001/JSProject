44281d0873b5b903781b9b47b35fa78666a93dca
skaushal001 <skaushalsingh281@gmail.com>
2024-06-02 01:28:05 +0530

    Update Kaushal.js

--- Kaushal.js
+++ Kaushal.js
@@ -1,6 +1,21 @@
 
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+// create a variable to hold your NFT's
 
 let numberofNFTs=0;
+
+/* this function will take in some values as parameters, create an
+   NFT object using the parameters passed to it for its metadata, 
+    and store it in the variable above.*/
+
 function mintNFT (name,age,gender){
 
 const nft= {
@@ -10,26 +25,28 @@ const nft= {
 
 };
 
-
 numberofNFTs++;
 return nft;
 
 }
 
 const nftArray =[];
-
+//Create an NFT and store it in an array 
 const firstNFT = mintNFT("Kaushal","19","Male");
 nftArray.push(firstNFT);
 
-
+//Create another NFT and store it in an array 
 
 const secondNFT = mintNFT("Hazelwood","25","Male");
 nftArray.push(secondNFT);
 
+//Create another NFT and store it in an array 
 
 const thirdNFT = mintNFT("Suzain","18","Female");
 nftArray.push(thirdNFT);
 
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
 
 function listNFTs(){
 
@@ -50,13 +67,21 @@ console.log("..........................");
 
 }
 
-function getTotalSupply(){
+// print the total number of NFTs we have minted to the console
 
+function getTotalSupply(){
 
 console.log("Total no. of NFTs" + numberofNFTs);
 
 }
 
+// call your functions below this line
+
+//Print the list of NFTs
+
 listNFTs();
 
+//Print the Total Supply
+
 getTotalSupply();
+
